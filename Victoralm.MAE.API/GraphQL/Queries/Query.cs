57d4cfb52d9e9416a19e36e6e1716cc87df1df01
Victoralm <Victoralm@users.noreply.github.com>
2024-11-28 13:33:34 -0300

    Code cleaning and adding Get endpoints for the other entities

--- Victoralm.MAE.API/GraphQL/Queries/Query.cs
+++ Victoralm.MAE.API/GraphQL/Queries/Query.cs
@@ -8,13 +8,21 @@ public class Query
     [UseProjection]
     [UseFiltering]
     [UseSorting]
+    public IQueryable<Appointment> GetAppointments([Service] PostgreContext context) => context.Appointments;
+    
+    [UseProjection]
+    [UseFiltering]
+    [UseSorting]
     public IQueryable<Medic> GetMedics([Service] PostgreContext context) => context.Medics;
+
+    [UseProjection]
+    [UseFiltering]
+    [UseSorting]
+    public IQueryable<MedicalSpecialty> GetMedicalSpecialties([Service] PostgreContext context) => context.MedicalSpecialties;
 
     [UsePaging]
     [UseProjection]
     [UseFiltering]
     [UseSorting]
     public IQueryable<Patient> GetPatients([Service] PostgreContext context) => context.Patients;
-
-    public string Teste => "Só um texto de teste";
 }

23977b2177b3288cb390c3660c79acc454d6c329
Victoralm <Victoralm@users.noreply.github.com>
2024-11-28 15:27:03 -0300

    Adding Migrations folder

---
+++ Victoralm.MAE.API/Migrations/20241128175539_Init.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Victoralm.MAE.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dev");
+
+            migrationBuilder.CreateTable(
+                name: "MedicalSpecialties",
+                schema: "dev",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MedicalSpecialties", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Patients",
+                schema: "dev",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false),
+                    Phone = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Medics",
+                schema: "dev",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false),
+                    Phone = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    MedicalSpecialtyId = table.Column<List<Guid>>(type: "uuid[]", nullable: false),
+                    MedicalSpecialtyId1 = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medics", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Medics_MedicalSpecialties_MedicalSpecialtyId1",
+                        column: x => x.MedicalSpecialtyId1,
+                        principalSchema: "dev",
+                        principalTable: "MedicalSpecialties",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Appointments",
+                schema: "dev",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Schedule = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    PatientId = table.Column<Guid>(type: "uuid", nullable: false),
+                    MedicId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Appointments_Medics_MedicId",
+                        column: x => x.MedicId,
+                        principalSchema: "dev",
+                        principalTable: "Medics",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Appointments_Patients_PatientId",
+                        column: x => x.PatientId,
+                        principalSchema: "dev",
+                        principalTable: "Patients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_MedicId",
+                schema: "dev",
+                table: "Appointments",
+                column: "MedicId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_PatientId",
+                schema: "dev",
+                table: "Appointments",
+                column: "PatientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Medics_MedicalSpecialtyId1",
+                schema: "dev",
+                table: "Medics",
+                column: "MedicalSpecialtyId1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Appointments",
+                schema: "dev");
+
+            migrationBuilder.DropTable(
+                name: "Medics",
+                schema: "dev");
+
+            migrationBuilder.DropTable(
+                name: "Patients",
+                schema: "dev");
+
+            migrationBuilder.DropTable(
+                name: "MedicalSpecialties",
+                schema: "dev");
+        }
+    }
+}
